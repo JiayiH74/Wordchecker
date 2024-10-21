a2dd0ca1c0fb94370ebc8135880f4fa284255d2c
JiayiH74 <jiayih12@nycstudents.net>
2024-10-21 15:50:41 +0000

    update

--- Main.java
+++ Main.java
@@ -1,1 +1,24 @@
+import java.util.ArrayList;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        WordChecker x = new WordChecker(words);
+        ArrayList<String> words = new ArrayList<String>();
+        words.add("an");
+        words.add("band");
+        words.add("band");
+        words.add("abandon");
+        System.out.println(words);
+        boolean wordChain = true;
+        System.out.println(wordChain);
 
+        WordChecker w = new WordChecker();
+        ArrayList<String> secwords = new ArrayList<String>();
+        secwords.add("to");
+        secwords.add("too");
+        secwords.add("stool");
+        secwords.add("tools");
+    }
+    
+}
